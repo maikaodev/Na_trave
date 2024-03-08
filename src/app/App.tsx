f950d3daf33fc0d7c43cf9e16e09502246a35f35
maikaodev <maikao.dev@gmail.com>
2024-03-07 21:29:02 -0300

    First commit

---
+++ src/app/App.tsx
@@ -0,0 +1,46 @@
+import background_homepage from '../assets/background-homepage.png';
+
+export function App() {
+  return (
+    <div className="bg-red-900 h-screen w-screen flex flex-col xl:flex-row justify-center items-center space-x-20">
+      <img
+        src={background_homepage}
+        alt="Duas pessoas com camisa de time"
+        className="w-80 md:w-1/2 max-w-[486px]"
+      />
+      <div className="w-80 md:w-1/2 max-w-[472px]">
+        <h1 className="text-2xl xl:text-3xl font-bold text-white ">
+          Dê o seu palpite na {<br />} Copa do Mundo do Catar 2022!{' '}
+        </h1>
+
+        <section className="flex flex-col space-y-4 mt-8 max-w-[472]">
+          <a
+            href="#"
+            className="min-h-12 bg-white text-center hover:text-red-900 px-2 py-4 rounded-2xl text-red-900 font-bold hover:scale-105 duration-500 "
+          >
+            Criar minha conta
+          </a>
+          <a
+            href="#"
+            className="min-h-12 bg-transparent text-center px-2 py-4 rounded-2xl text-white hover:text-white font-bold border-2 border-white hover:scale-105 duration-500"
+          >
+            Fazer login
+          </a>
+        </section>
+      </div>
+
+      <footer className="mt-10 absolute bottom-10">
+        <span className="text-white">
+          Desenvolvido por{' '}
+          <a
+            href="https://www.linkedin.com/in/maikaodev/"
+            className="text-white hover:text-green-500 duration-500"
+          >
+            @maikaodev
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
+}
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#0B0E16',
+        white: '#F4F6FF',
+        grey: {
+          900: '#696C74',
+          500: '#91949D',
+          300: '#B1B4BD',
+        },
+        red: {
+          900: '#300219',
+          500: '#AF053F',
+          300: '#BB2E57',
+        },
+      },
+      fontSize: {
+        xs: ['12px', '16px'],
+        sm: ['14px', '18px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', ',32px'],
+        '2xl': ['32px', '40px'],
+        '3xl': ['48px', '56px'],
+      },
+    },
+  },
+  plugins: [],
+};
+
